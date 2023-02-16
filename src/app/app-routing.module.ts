02617ae5530089ecd9a5ec50c499428bffee5535
NadhiyaNishad <nadhiyanishad460@gmail.com>
2023-02-17 00:26:47 +0530

    works3

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,32 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { ArtformComponent } from './pages/artform/artform.component';
+import { ContactusComponent } from './pages/contactus/contactus.component';
+import { HomepageComponent } from './pages/homepage/homepage.component';
+import { LoginComponent } from './pages/login/login.component';
+import { SignupComponent } from './pages/signup/signup.component';
+import { SinglepageComponent } from './pages/singlepage/singlepage.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path:'',component:HomepageComponent
+  },
+  {
+    path:'artform',component:ArtformComponent
+  },
+  {
+    path:'contactus',component:ContactusComponent
+  },
+  {
+    path:'login',component:LoginComponent
+  },
+  {
+    path:'signup',component:SignupComponent
+  },
+  {
+    path:'single',component:SinglepageComponent
+  },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
---
+++ src/assets/data.ts
@@ -0,0 +1,72 @@
+export const data=[
+    {
+    id:1,
+    name:"Madhubani",
+    image:"madhubani.webp",
+    clk:"ClickHere",
+    desc:"This art is also known as Mithila art, and it originated in the kingdom of Janak in Nepal and in present-day Bihar."
+},
+{
+    id:2,
+    name:"Warli",
+    image:"warli.webp",
+    clk:"ClickHere",
+    desc:"This form of art dates back to 2500 BCE, and it was practiced by the Warli tribes from Thane and Nasik in Maharashtra."
+},
+{
+    id:3,
+    name:"Miniature",
+    image:"miniature.jpg",
+    clk:"ClickHere",
+    desc:"The miniature paintings illustrate a combination of Indian, Islamic and Persian art styles. This art form dates back to"
+},
+{
+    id:4,
+    name:"Kalamkari",
+    image:"kalamkari.jpg",
+    clk:"ClickHere",
+    desc:"Having a strong connection to Persian motifs, this art has been in practice for more than 3000 years. Kalamkari derives it"
+},
+{
+    id:5,
+    name:"Tanjore",
+    image:"tanjore.jpg",
+    clk:"ClickHere",
+    desc:"First painted in the 16th century under the Chola regime, this painting originated in the Thanjavur district in Tamil Nadu."
+},
+{
+    id:6,
+    name:"Pattachitra",
+    image:"pattachira.jpg",
+    clk:"ClickHere",
+    desc:"Pattachitra art forms date back to the 5th century and originated in Odisha and West Bengal. These cloth-based scroll paintings"
+},
+{
+    id:7,
+    name:"Gond Painting",
+    image:"gond.jpg",
+    clk:"ClickHere",
+    desc:"A native art form of Madhya Pradesh, Gond paintings are mostly themed on animals and birds. Practised by the Gond tribes, this"
+},
+{
+    id:8,
+    name:"Kalighat",
+    image:"3.jpg",
+    clk:"ClickHere",
+    desc:"This form of painting originated in the 19th century in Bengal. These paintings were made on cloth or patas and often depicted"
+},
+{
+    id:9,
+    name:"Phad",
+    image:"phad.jpg",
+    clk:"ClickHere",
+    desc:"Dating back thousands of years, Phad is a narrative scroll painting tradition which originated in Rajasthan. Red, yellow and"
+},
+{
+    id:10,
+    name:"Cheriyal Scrolls",
+    image:"cheriyal.jpg",
+    clk:"ClickHere",
+    desc:"This form of art originated in present-day Telangana and has been practised by the Nakashi family through generations.The Kalamkari"
+},
+]
